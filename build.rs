ed86aad8ce2ac579b9b55991a35fb60e0c177704
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2022-12-29 10:55:28 +0100

    build: use pkg version if repo opening failed

--- build.rs
+++ build.rs
@@ -31,38 +31,41 @@ fn dev_version() -> String {
 
 fn version() -> Result<String, Error> {
     let manifest_dirpath = Path::new(env!("CARGO_MANIFEST_DIR"));
-    let repo = Repository::open(manifest_dirpath)?;
-    let diff = repo.diff_index_to_workdir(None, None)?;
-    if diff.deltas().count() > 0 {
-        Ok(dev_version())
-    } else {
-        let tag_regex = Regex::new(TAG_PATTERN).unwrap();
-        let head_commit_id = repo.head()?.peel_to_commit()?.id();
-        let tag_names = repo.tag_names(None)?;
-        let captures = tag_names.iter().flatten().find_map(|tag_name| {
-            tag_regex.captures(tag_name).and_then(|captures| {
-                match repo.find_reference(&format!("refs/tags/{}", tag_name)) {
-                    Ok(tag) => tag.target().and_then(|target_id| {
-                        if target_id == head_commit_id {
-                            Some(captures)
-                        } else {
+    if let Ok(repo) = Repository::open(manifest_dirpath) {
+        let diff = repo.diff_index_to_workdir(None, None)?;
+        if diff.deltas().count() > 0 {
+            Ok(dev_version())
+        } else {
+            let tag_regex = Regex::new(TAG_PATTERN).unwrap();
+            let head_commit_id = repo.head()?.peel_to_commit()?.id();
+            let tag_names = repo.tag_names(None)?;
+            let captures = tag_names.iter().flatten().find_map(|tag_name| {
+                tag_regex.captures(tag_name).and_then(|captures| {
+                    match repo.find_reference(&format!("refs/tags/{}", tag_name)) {
+                        Ok(tag) => tag.target().and_then(|target_id| {
+                            if target_id == head_commit_id {
+                                Some(captures)
+                            } else {
+                                None
+                            }
+                        }),
+                        Err(err) => {
+                            println!("cargo:warning=Unable to fetch `{}` tag", tag_name);
+                            println!("cargo:warning=git: {}", err);
                             None
                         }
-                    }),
-                    Err(err) => {
-                        println!("cargo:warning=Unable to fetch `{}` tag", tag_name);
-                        println!("cargo:warning=git: {}", err);
-                        None
                     }
-                }
-            })
-        });
-        if let Some(captures) = captures {
-            let version = captures.get(1).unwrap().as_str();
-            Ok(version.into())
-        } else {
-            let sha = head_commit_id.to_string();
-            Ok(format!("{}+{}", PKG_VERSION, &sha[0..7]))
+                })
+            });
+            if let Some(captures) = captures {
+                let version = captures.get(1).unwrap().as_str();
+                Ok(version.into())
+            } else {
+                let sha = head_commit_id.to_string();
+                Ok(format!("{}+{}", PKG_VERSION, &sha[0..7]))
+            }
         }
+    } else {
+        Ok(PKG_VERSION.into())
     }
 }
